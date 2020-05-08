901ef3e1954e72ea5c20c1dcd044b0ec6ac8555d
NickALoyd <54961013+NickALoyd@users.noreply.github.com>
2020-05-08 16:36:52 -0500

    Update happyNumbers.py
    This was the first one I tried and I completely failed. Tried again and it only took a couple of minutes

--- happyNumbers.py
+++ happyNumbers.py
@@ -1,25 +1,27 @@
-#happy numbers
 #https://www.youtube.com/watch?v=kC6YObu61_w
-'''
-    Rules of happy numbers
-    Split number into x digits
-    square each digit then add up
+#To find happy numbers you
+#Split the number into all of the digits
+#square and add all the digits and repeat
 
-    if terminate to 1 happy number
-    if repeated unhappy number
-'''
-def happyNumbers(x):
-    if x == 1:
-        return ("Happy")    
-    strX = str(x)
-    xSquared = []
-    for i in range(len(strX)):
-        xSquared.append(int(strX[0+i:1+i])**2)    
-    x = xSquared
+#if you repeat a number it is going to be an unhappy number
+#if you get to 1 then it is a happy number
+def happyNumber(y):
+    if y == 1: #if 1 then it terminates and is a happy number
+        return True
+    y = str(y) #This splits the number
     total = 0
-    for i in range(len(x)):
-        total += x[i]
-    print (total)
-    return happyNumbers(total)
-x = 97
-print(happyNumbers(x))
+    for i in range(len(y)):
+        total += int(y[i])**2 #adds the square
+    if total in visitedNumbers: #if seen before then it will just loop
+        return False
+    visitedNumbers.append(total) 
+    return happyNumber(total)
+
+
+happyNumbers = []
+for i in range(100):
+    visitedNumbers = []
+    hNT = happyNumber(i) #happy number test
+    if hNT:
+        happyNumbers.append(i)
+print(happyNumbers)
