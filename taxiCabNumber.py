68099375beeabd9b271a18fa710f463c03101d05
NickALoyd <54961013+NickALoyd@users.noreply.github.com>
2020-05-09 14:36:00 -0500

    Create taxiCabNumber.py
    Prints out the numbers and what the cube roots are
    might want to change the minimum of what it can be with the minimum to have improved speed

---
+++ taxiCabNumber.py
@@ -0,0 +1,19 @@
+#https://www.youtube.com/watch?v=LzjaDKVC4iY
+#a number that can be represented as 2 cubes in 2 diffrent ways
+def isTaxiCab(goal):
+    Max = int(goal**(1/3)) + 1
+    l = []
+    for x in range(Max):
+        for y in range(Max):
+            if x**3 + y**3 == goal:
+                l.append([x,y])
+    if len(l) >= 4:
+        print(l[0:int(len(l)/2)])
+        return True
+    return False
+    
+goal = 1729
+
+for i in range(1,5000):
+    if isTaxiCab(i):
+        print(i)
