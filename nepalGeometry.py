1b378232232cd392f527ac9fa1b9bbaffd3aafb5
NickALoyd <54961013+NickALoyd@users.noreply.github.com>
2020-06-08 17:40:47 -0400

    Returning Classes when possible
    Also deleted Code on the bottom to test changes
    Also also added a name for Points that is optional for debugging purposes

--- nepalGeometry.py
+++ nepalGeometry.py
@@ -13,31 +13,35 @@ class Polynomial():
 		return Polynomial(*(x+y for x,y in zip(self.coeffs, other.coeffs)))
 
 class Point():
-	def __init__(self, x,y=None):
+	def __init__(self, x,y=None,name=''): #Added an optinal C for the Points Name
 		if y == None:
 			self.x = float(x[0])
 			self.y = float(x[1])
 		else:
 			self.x = float(x)
 			self.y = float(y)
+		self.name = name
+	def getPos(self):		 	
+ 		return (self.x,self.y)
 
 	def __repr__(self):
-		return 'Point({},{})'.format(self.x,self.y)
+		return '{}:({},{})'.format(self.name, self.x,self.y)
 	
 	def __add__(self,other):
 		return Line(self,other)
 
 class Circle():
-	def __init__(self,radius,center):
+	def __init__(self,radius,center,c=''):
 		self.r = radius
 		self.center = center
 		self.x = self.center.x
 		self.y = self.center.y
 
 class Line():
-	def __init__(self,p1,p2):
+	def __init__(self,p1,p2,name=''):
 		self.p1 = p1
 		self.p2 = p2
+		self.name = p1.name + p2.name
 
 	def slope(self):
 		if self.p2.x-self.p1.x == 0:
@@ -77,7 +81,10 @@ class Line():
 	def midpoint(self):
 		x = (self.p1.x + self.p2.x)/2.0
 		y = (self.p1.y + self.p2.y)/2.0
-		return x,y
+		return Point(x,y)
+
+	def __repr__(self):
+		return 'Line {}: ({}), ({})'.format(self.name, self.p1,self.p2)
 	
 	def __add__(self,other):
 		#I need to solve this when slopes are vertical
@@ -86,18 +93,18 @@ class Line():
 			#print("Case 1: ")
 			x = self.p1.x
 			y = other.slope() * x + other.yInter()
-			return x,y
+			return Point(x,y)
 
 		if other.slope() == None:
 			#print("Case 2: ")
 			x = other.p1.x
 			y = self.slope() * x + self.yInter()
-			return x,y
+			return Point(x,y)
 
 		#print("Case 3: ")
 		x = (other.yInter() - self.yInter())/(self.slope() - other.slope())
 		y = self.slope() * x + self.yInter()
-		return x,y
+		return Point(x,y)
 
 def hypotenuse(A,B):
 		return (A**2 + B**2)**(1.0/2.0) 
@@ -113,7 +120,7 @@ def intersectionOf2Circles(A,B):
 		A.y + x * ey + y * ex)
 		P2 = (A.x + x * ex + y * ey,
 		A.y + x * ey - y * ex)
-		return (P1,P2)
+		return (Point(P1),Point(P2))
 	return "NONE" # No Intersection, far outside or one circle within the other'''
 
 def findShortestPointOfLinePoint(l,p, min, max):
@@ -145,15 +152,5 @@ def intersectionsOfLineCircle(l,c): #https://www.youtube.com/watch?v=esf5lMkQT7k
 	#print("h,k,r,s,b",h,k,r,s,b)
 	p1y = l.getYof(p1x)	
 	p2y = l.getYof(p2x)
-	return (p1x,p1y),(p2x,p2y)
-
-p1 = Point(2,1)
-p2 = Point(4,2)
-l1 = p1+p2
-
-p1 = Point(0,1)
-p2 = Point(0,0)
-l2 = p1+p2
-
-print(l1+l2)
+	return Point(p1x,p1y),Point(p2x,p2y)
 
