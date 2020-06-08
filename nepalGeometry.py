56afe7532050895cdde126ddef04580bf4540e86
NickALoyd <54961013+NickALoyd@users.noreply.github.com>
2020-06-08 16:36:52 -0400

    Fundamental Changes
    You can add 2 points to form a line
    You can add 2 Lines to get the intersection point
    
    This is a massive change that is going to make it easier to call in the Nepal Flag project

--- nepalGeometry.py
+++ nepalGeometry.py
@@ -2,6 +2,15 @@
 import math
 import numpy as np
 
+class Polynomial():
+	def __init__(self, *coeffs):
+		self.coeffs = coeffs
+
+	def __repr__(self):
+		return 'Polynomial(*{!r})'.format(self.coeffs)
+
+	def __add__(self,other):
+		return Polynomial(*(x+y for x,y in zip(self.coeffs, other.coeffs)))
 
 class Point():
 	def __init__(self, x,y=None):
@@ -9,11 +18,14 @@ class Point():
 			self.x = float(x[0])
 			self.y = float(x[1])
 		else:
-			self.x = x
-			self.y = y
+			self.x = float(x)
+			self.y = float(y)
 
-	def getPos(self):
-		return (self.x,self.y)
+	def __repr__(self):
+		return 'Point({},{})'.format(self.x,self.y)
+	
+	def __add__(self,other):
+		return Line(self,other)
 
 class Circle():
 	def __init__(self,radius,center):
@@ -48,9 +60,9 @@ class Line():
 
 	def getYof(self,x): #y = mx+b
 		return self.slope() * x + self.yInter()
+
 	def getXof(self,y): #mx = y-b
 		return y - self.yInter()
-
 
 	def distance(self):
 		a = self.p1.x - self.p2.x
@@ -65,6 +77,26 @@ class Line():
 	def midpoint(self):
 		x = (self.p1.x + self.p2.x)/2.0
 		y = (self.p1.y + self.p2.y)/2.0
+		return x,y
+	
+	def __add__(self,other):
+		#I need to solve this when slopes are vertical
+		if self.slope() == None:
+			#if slope A has a vertical and the other is normal
+			#print("Case 1: ")
+			x = self.p1.x
+			y = other.slope() * x + other.yInter()
+			return x,y
+
+		if other.slope() == None:
+			#print("Case 2: ")
+			x = other.p1.x
+			y = self.slope() * x + self.yInter()
+			return x,y
+
+		#print("Case 3: ")
+		x = (other.yInter() - self.yInter())/(self.slope() - other.slope())
+		y = self.slope() * x + self.yInter()
 		return x,y
 
 def hypotenuse(A,B):
@@ -84,27 +116,6 @@ def intersectionOf2Circles(A,B):
 		return (P1,P2)
 	return "NONE" # No Intersection, far outside or one circle within the other'''
 
-def intersectionOf2Lines(A,B):
-	#I need to solve this when slopes are vertical
-	if A.slope() == None:
-		#if slope A has a vertical and the other is normal
-		#print("Case 1: ")
-		x = A.p1.x
-		y = B.slope() * x + B.yInter()
-		return x,y
-
-	if B.slope() == None:
-		#print("Case 2: ")
-		x = B.p1.x
-		y = A.slope() * x + A.yInter()
-		return x,y
-
-	#print("Case 3: ")
-	x = (B.yInter() - A.yInter())/(A.slope() - B.slope())
-	y = A.slope() * x + A.yInter()
-	return x,y
-
-
 def findShortestPointOfLinePoint(l,p, min, max):
 	smallestDistance = 10**100
 	shortestPos = 0
@@ -116,31 +127,33 @@ def findShortestPointOfLinePoint(l,p, min, max):
 		if (d < smallestDistance):
 			smallestDistance = d
 			shortestPos= x,y
-	print(shortestPos)
+	#print(shortestPos)
 	return smallestDistance
 
-def quadradicFormula(a,b,c):
-	p1 = (-b + (b**2 - 4*a*c)**(1.0/2.0))/(2*a)
-	p2 = (-b - (b**2 - 4*a*c)**(1.0/2.0))/(2*a)
-	return p1,p2
-
-def intersectionsOfLineCircle(l,c):
+def intersectionsOfLineCircle(l,c): #https://www.youtube.com/watch?v=esf5lMkQT7k
+	#This now works assuming the line is zero for some reason IDK
 	#(x+h)**2 + (y+k)**2 = r**2, y = s*x+b  wolframalpha plot of a circle, and plot of line
 	h,k,r = c.x,c.y,c.r
 	s,b = l.slope(),l.yInter()
-	print("h,k,r,s,b",h,k,r,s,b)
 
-	p1x = -(-h - k*s - b*s - (-k**2 - 2*k*b - b**2 + r**2 + 2*h*k*s + 2*h*b*s - h**2*s**2 + r**2*s**2)**(1.0/2.0))/(1 + s**2)
-	p1y = l.getYof(p1x)
-
-	p2x = -(-h - b*s - k*s + (-b**2 - 2*b*k - k**2 + r**2 + 2*b*h*s + 2*h*k*s - h**2*s**2 + r**2*s**2)**(1.0/2.0))/(1 + s**2)
+	if s == 0:
+		p1x = h-(-b**2 + 2*b*k - k**2 + r**2)**(1.0/2.0)
+		p2x = h+(-b**2+2*b*k- k**2 + r**2)**(1.0/2.0)
+	else:
+		p1x = -(-h - k*s - b*s - (-k**2 - 2*k*b - b**2 + r**2 + 2*h*k*s + 2*h*b*s - h**2*s**2 + r**2*s**2)**(1.0/2.0))/(1 + s**2)
+		p2x = -(-h - b*s - k*s + (-b**2 - 2*b*k - k**2 + r**2 + 2*b*h*s + 2*h*k*s - h**2*s**2 + r**2*s**2)**(1.0/2.0))/(1 + s**2)
+	#print("h,k,r,s,b",h,k,r,s,b)
+	p1y = l.getYof(p1x)	
 	p2y = l.getYof(p2x)
-
 	return (p1x,p1y),(p2x,p2y)
-	
-p1 = Point([0,0])
-p2 = Point([1,2])
-l = Line(p1,p2)
-c = Circle(4,Point([2,7]))
 
-print(intersectionsOfLineCircle(l,c))
+p1 = Point(2,1)
+p2 = Point(4,2)
+l1 = p1+p2
+
+p1 = Point(0,1)
+p2 = Point(0,0)
+l2 = p1+p2
+
+print(l1+l2)
+
